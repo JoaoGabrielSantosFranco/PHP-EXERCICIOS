68fad3d364bdca16d26493ec8febf3a4bb4c55fd
Joao Gabriel Franco <80074087+JoaoGabrielSantosFranco@users.noreply.github.com>
2023-04-10 13:58:35 -0300

    modificando o  iconv e replace por um metodo mais efetivo

--- Lista/ex3.php
+++ Lista/ex3.php
@@ -10,18 +10,18 @@
 
 <body>
     <h1>
+        <pre>
         <?php
 
         // Escreva uma função que retorne à quantidade de caracteres da última palavra em uma frase
         function countCharactersLastSentence($sentence)
         {
-            $sentence = iconv('UTF-8', 'ASCII//TRANSLIT', $sentence);
-            $sentence = preg_replace('/[^a-zA-Z0-9]/', ' ', $sentence);
+            $sentence = iconv("UTF-8", "ISO-8859-1", $sentence);
             $sentence = str_split($sentence);
 
             $characters = 0;
             for ($i = count($sentence); $i > 0; $i--) {
-                if ($sentence[$i - 1] == ' ') {
+                if ($sentence[$i - 1] === ' ') {
                     return ($characters);
                 }
                 $characters += 1;
@@ -29,10 +29,12 @@
             return $characters;
         }
 
-        $string = "Estou praticando exercicios";
+        $string = "Estou praticando exercícios";
         echo countCharactersLastSentence($string);
         ?>
+            </pre>
     </h1>
+
 </body>
 
 </html>
